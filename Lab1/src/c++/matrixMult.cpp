acc7a6818bcef5ac19de03eca47dea8831e24978
Mike Pinto <msp4ever@gmail.com>
2015-03-24 23:46:17 +0000

    c, c++ versions updated, now build x64 processes with 3GB allocated for high memory ops

--- Lab1/src/c++/matrixMult.cpp
+++ Lab1/src/c++/matrixMult.cpp
@@ -2,47 +2,60 @@
 //
 
 #include "stdafx.h"
+#pragma comment(linker, "/STACK:3246391296")
+#define matrixSize 10000
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double **A, **B, **C;
-	int l, c;
-	cout << "Lines x Columns?\n> ";
+	double A[matrixSize][matrixSize],
+		B[matrixSize][matrixSize],
+		C[matrixSize][matrixSize];
+	int l=10000, c=10000, i, j, k;
+	/*cout << "Lines x Columns?\n> ";
 	
 	cin >> l >> c;
 
-	cout << "lines: " << l << " cols: " << c << endl;
-
 	A = new double*[l];
 	B = new double*[l];
-	C = new double*[c];
+	C = new double*[l];
 
-	for (int i = 0; i < l; i++){
+	for (i = 0; i < l; i++){
 		A[i] = new double[c];
 	}
 
-	for (int i = 0; i < l; i++){
+	for (i = 0; i < l; i++){
 		B[i] = new double[c];
 	}
 
-	for (int i = 0; i < c; i++){
-		C[i] = new double[l];
-	}
+	for (i = 0; i < l; i++){
+		C[i] = new double[c];
+	}*/
 
-	for (int i = 0; i < l; i++){
+	cout << "lines: " << l << " cols: " << c << endl;
+
+	for (i = 0; i < l; i++){
 		for (int j = 0; i < l; i++){
 			A[i][j] = 1.0;
 		}
 	}
 
-	for (int i = 0; i < l; i++){
+	for (i = 0; i < l; i++){
 		for (int j = 0; i < l; i++){
 			B[i][j] = 1.0+i;
 		}
 	}
 
-
+	for (i = 0; i<matrixSize; i++)
+	{
+		for (k = 0; k<matrixSize; k++)
+		{
+			for (j = 0; j < matrixSize; j++)
+			{
+				C[i][j] = C[i][j] + A[i][k] * B[k][j];
+			}
+		}
+	}
 
 	system("PAUSE");
 	return 0;
