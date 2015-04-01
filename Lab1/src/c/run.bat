f225d69f2049c016d98e26d291fc039fd9c1ee1f
Mike Pinto <msp4ever@gmail.com>
2015-04-01 16:35:57 +0100

    code final

--- Lab1/src/c/run.bat
+++ Lab1/src/c/run.bat
@@ -1,26 +1,72 @@
 @echo off
+set threads=2
+set test=echo Test
+set time=time /T
 call :sub > execution.txt
 exit /b
 
 :sub
-".\\x64\\Release\\product.exe" 600 600 2 
+%time% > CON
+".\\x64\\Release\\product.exe" 600 600 %threads%
+%time% > CON
+%test% 600 DONE > CON
 echo --------------------- 
-".\\x64\\Release\\product.exe" 1000 1000 2 
+
+%time% > CON
+".\\x64\\Release\\product.exe" 1000 1000 %threads%
+%time% > CON
+%test% 1000 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 1400 1400 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 1400 1400 %threads%
+%time% > CON
+%test% 1400 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 1800 1800 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 1800 1800 %threads%
+%time% > CON
+%test% 1800 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 2200 2200 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 2200 2200 %threads%
+%time% > CON
+%test% 2200 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 2600 2600 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 2600 2600 %threads%
+%time% > CON
+%test% 2600 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 3000 3000 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 3000 3000 %threads%
+%time% > CON
+%test% 3000 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 4000 4000 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 4000 4000 %threads%
+%time% > CON
+%test% 4000 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 4000 4000 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 6000 6000 %threads%
+%time% > CON
+%test% 6000 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 8000 8000 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 8000 8000 %threads%
+%time% > CON
+%test% 8000 DONE > CON
 echo ---------------------
-".\\x64\\Release\\product.exe" 10000 10000 2
+
+%time% > CON
+".\\x64\\Release\\product.exe" 10000 10000 %threads%
+%time% > CON
+%test% 10000 DONE > CON
