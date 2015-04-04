c7cd54602548df99efd87c32c5116841301ae171
Mike Pinto <msp4ever@gmail.com>
2015-04-04 22:20:31 +0100

    code update

--- Lab1/src/c/prodesc.cpp
+++ Lab1/src/c/prodesc.cpp
@@ -55,6 +55,74 @@ int DifTime(SYSTEMTIME &Time1, SYSTEMTIME &Time2)
 	return(total);
 }
 
+void sequential_normal(int m_ar, int m_br, CdMatriz &A, CdMatriz &B, CdMatriz &C){
+	int i, j, k;
+	double temp;
+
+	for (i = 0; i<m_ar; i++)
+	{
+		for (j = 0; j<m_br; j++)
+		{
+			temp = 0;
+			for (k = 0; k<m_ar; k++)
+			{
+				temp += +A(i, k) * B(k, j);
+			}
+			C(i, j, temp);
+		}
+	}
+}
+
+void sequential_optimized(int m_ar, int m_br, CdMatriz &A, CdMatriz &B, CdMatriz &C){
+	int i, j, k;
+
+	for (i = 0; i<m_ar; i++)
+	{
+		for (k = 0; k<m_br; k++)
+		{
+			for (j = 0; j<m_ar; j++)
+			{
+				C(i, j, C(i, j) + A(i, k) * B(k, j));
+			}
+		}
+	}
+}
+
+void open_mp_normal(int m_ar, int m_br, CdMatriz &A, CdMatriz &B, CdMatriz &C, const int nt){
+	int i, j, k;
+	double temp;
+
+	#pragma omp parallel for shared(A,B,C) private(k,j) num_threads(nt) // 1...4 threads
+	for (i = 0; i<m_ar; i++)
+	{
+		for (j = 0; j<m_br; j++)
+		{
+			temp = 0;
+			for (k = 0; k<m_ar; k++)
+			{
+				temp += + A(i, k) * B(k, j);
+			}
+			C(i, j, temp);
+		}
+	}
+}
+
+void open_mp_optimized(int m_ar, int m_br, CdMatriz &A, CdMatriz &B, CdMatriz &C, const int nt){
+	int i, j, k;
+	
+	#pragma omp parallel for shared(A,B,C) private(k,j) num_threads(nt) // 1...4 threads
+	for (i = 0; i<m_ar; i++)
+	{
+		for (k = 0; k<m_br; k++)
+		{
+			for (j = 0; j<m_ar; j++)
+			{
+				C(i, j, C(i, j) + A(i, k) * B(k, j));
+			}
+		}
+	}
+}
+
 void threadMult(thread_args args) {
 	int i, j, k;
 	for (i = args.posi; i<args.posf; i++)
@@ -69,7 +137,29 @@ void threadMult(thread_args args) {
 	}
 }
 
-void OnMult(int m_ar, int m_br, const int nt) 
+void optimized_explicit_thread_parallel(int m_ar, int m_br, CdMatriz &A, CdMatriz &B, CdMatriz &C, const int nt){
+	const int nr_threads = nt; //1...4
+	const int n_per_thread = m_br / nr_threads;
+
+	vector<thread> threadList;
+
+	for (int tn = 0; tn < nr_threads; tn++){
+		thread_args t_args;
+		t_args.posi = tn * n_per_thread;
+		t_args.posf = (1 + tn)*n_per_thread;
+		t_args.size = m_br;
+		t_args.A = &A;
+		t_args.B = &B;
+		t_args.C = &C;
+		threadList.push_back(thread(threadMult, t_args));
+	}
+
+	for (int tn = 0; tn < nr_threads; tn++){
+		threadList[tn].join();
+	}
+}
+
+void OnMult(int m_ar, int m_br, const int nt, int option) 
 {
 	// TODO: Add your command handler code here
 	SYSTEMTIME Time1, Time2;
@@ -109,52 +199,35 @@ void OnMult(int m_ar, int m_br, const int nt)
 
 		GetLocalTime(&Time1);
 
-		// OpenMP version 
-		/*#pragma omp parallel for shared(A,B,C) private(k,j) num_threads(nt) // 1...4 threads
-		for(i=0; i<m_ar; i++)
-		{	for( k=0; k<m_br; k++)
-			{	
-				for( j=0; j<m_ar; j++)
-				{	
-					C(i, j, C(i,j) + A(i, k) * B(k, j));
-				}
-			}
-		}*/
-
-		/* ------ Explicit Thread version ------ */
-		
-		const int nr_threads = nt; //1...4
-		const int n_per_thread = m_br / nr_threads;
-		
-		vector<thread> threadList;
-
-		for (int tn = 0; tn < nr_threads; tn++){
-			thread_args t_args;
-			t_args.posi = tn * n_per_thread;
-			t_args.posf = (1 + tn)*n_per_thread;
-			t_args.size = m_br;
-			t_args.A = &A;
-			t_args.B = &B;
-			t_args.C = &C;
-			threadList.push_back(thread (threadMult, t_args));
+		switch (option){
+		case 1:
+			// Sequential execution of original algorithm
+			sequential_normal(m_ar, m_br, A, B, C);
+			break;
+		case 2:
+			// Sequential execution of optimized algorithm
+			sequential_optimized(m_ar, m_br, A, B, C);
+			break;
+		case 3:
+			// OpenMP Normal version 
+			open_mp_normal(m_ar, m_br, A, B, C, nt);
+			break;
+		case 4:
+			// OpenMP Optimized version 
+			open_mp_optimized(m_ar, m_br, A, B, C, nt);
+			break;
+		case 5:
+			// Explicit Thread version
+			optimized_explicit_thread_parallel(m_ar, m_br, A, B, C, nt);
+			break;
+		default:
+			cout << "No Such Option" << endl;
 		}
 
-		for (int tn = 0; tn < nr_threads; tn++){
-			threadList[tn].join();
-		}
-		
 
 		GetLocalTime(&Time2);
 		sprintf(st, "Time: %3.3f seconds\n", ((float) DifTime(Time1, Time2)/1000.0));
 		cout << st;
-
-		/*
-		cout << "Result matrix: " << endl;
-		for(i=0; i<1; i++)
-		{	for(j=0; j<min(10,m_br); j++)
-				cout << C(i,j) << " ";
-		}
-		cout << endl;*/
 
 		GlobalUnlock(ha);
 		GlobalUnlock(hb);
@@ -280,40 +353,35 @@ int main (int argc, char *argv[])
 	double area, pi, x;
 	int i, n;
 	char c;
-	int lin, col, nt=1;
+	int lin, col, nt=1, opt=0;
 	int op;
+
+	if (argc < 2){
+		cout << "Use: product.exe <nr of lines> <nr of columns> <option> <?nr of threads>" << endl
+			<< "Options for Algorithm Execution:\n\t"
+			<< "1. Sequential - Original Version\n\t"
+			<< "2. Sequential - Optimized Version\n\t"
+			<< "3. Parallel (OpenMP)- Original Version\n\t"
+			<< "4. Parallel (OpenMP) - Optimized Version\n\t"
+			<< "5. Parallel (std::thread) - Optimized Version (for comparison with OpenMP)" << endl;
+
+		cout << "\nEnter to continue ...";
+		cin.get(c);
+
+		return 1;
+	}
 
 	cout <<  "Lines: " << argv[1] << endl
 		<< "Columns: " << argv[2] << endl
-		<< "Nr Threads: " << argv[3] << endl;
+		<< "Option: " << argv[3] << endl
+		<< "Nr Threads: " << argv[4] << endl;
+
 	lin = atoi(argv[1]);
 	col = atoi(argv[2]);
-	nt = atoi(argv[3]);
-
-	OnMult(lin, col, nt);
+	opt = atoi(argv[3]);
+	nt = atoi(argv[4]);
 	
-	op=1;
-	/*do {
-		cout << endl << "1. Multiplication" << endl;
-		cout << "2. Block Multiplication" << endl;
-		cout << "Selection?: ";
-		scanf_s("%d", &op);
-		if (op == 0)
-			break;
-		printf("Dimensions: lins cols ? ");
-   		scanf_s("%d %d", &lin, &col);
-
-
-		switch (op){
-			case 1: 
-				OnMult(lin, col, nt);
-				break;
-			case 2:
-				OnMultblock(lin, col, nt);
-				break;
-		}
-	}while (op != 0);
-	*/
+	OnMult(lin, col, nt, opt);
 
 	/*
 	cout << "Enter to continue ...";
--- Lab1/src/c/run.bat
+++ Lab1/src/c/run.bat
@@ -1,5 +1,6 @@
 @echo off
-set threads=2
+set threads=4
+set option=3
 set test=echo Test
 set time=time /T
 call :sub > execution.txt
@@ -7,66 +8,43 @@ exit /b
 
 :sub
 %time% > CON
-".\\x64\\Release\\product.exe" 600 600 %threads%
+".\\x64\\Release\\product.exe" 600 600 %option% %threads%
 %time% > CON
 %test% 600 DONE > CON
 echo --------------------- 
 
 %time% > CON
-".\\x64\\Release\\product.exe" 1000 1000 %threads%
+".\\x64\\Release\\product.exe" 1000 1000 %option% %threads%
 %time% > CON
 %test% 1000 DONE > CON
 echo ---------------------
 
 %time% > CON
-".\\x64\\Release\\product.exe" 1400 1400 %threads%
+".\\x64\\Release\\product.exe" 1400 1400 %option% %threads%
 %time% > CON
 %test% 1400 DONE > CON
 echo ---------------------
 
 %time% > CON
-".\\x64\\Release\\product.exe" 1800 1800 %threads%
+".\\x64\\Release\\product.exe" 1800 1800 %option% %threads%
 %time% > CON
 %test% 1800 DONE > CON
 echo ---------------------
 
 %time% > CON
-".\\x64\\Release\\product.exe" 2200 2200 %threads%
+".\\x64\\Release\\product.exe" 2200 2200 %option% %threads%
 %time% > CON
 %test% 2200 DONE > CON
 echo ---------------------
 
 %time% > CON
-".\\x64\\Release\\product.exe" 2600 2600 %threads%
+".\\x64\\Release\\product.exe" 2600 2600 %option% %threads%
 %time% > CON
 %test% 2600 DONE > CON
 echo ---------------------
 
 %time% > CON
-".\\x64\\Release\\product.exe" 3000 3000 %threads%
+".\\x64\\Release\\product.exe" 3000 3000 %option% %threads%
 %time% > CON
 %test% 3000 DONE > CON
 echo ---------------------
-
-%time% > CON
-".\\x64\\Release\\product.exe" 4000 4000 %threads%
-%time% > CON
-%test% 4000 DONE > CON
-echo ---------------------
-
-%time% > CON
-".\\x64\\Release\\product.exe" 6000 6000 %threads%
-%time% > CON
-%test% 6000 DONE > CON
-echo ---------------------
-
-%time% > CON
-".\\x64\\Release\\product.exe" 8000 8000 %threads%
-%time% > CON
-%test% 8000 DONE > CON
-echo ---------------------
-
-%time% > CON
-".\\x64\\Release\\product.exe" 10000 10000 %threads%
-%time% > CON
-%test% 10000 DONE > CON
